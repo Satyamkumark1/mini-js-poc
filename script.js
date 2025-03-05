f12e0ce4cc0bbdc2194b47ec30942a121bbdbafc
Satyamkumark1 <satyamnagar26@gmail.com>
2025-03-05 23:04:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = 'AIzaSyCxtR30DznLH8BL5hZwTsbEVSEALFEevKQ'; 
-// Replace with your actual Gemini API key – this stores the API key to authenticate requests to the Gemini API.
+
 
 const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
 // The base URL of the Gemini API used to generate content (for text-based responses).
